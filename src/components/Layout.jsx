add23b797d9548d5dcaae71005c4e0269ab0f5b7
lgiraldo-dev <lgiraldo@quanspire.com>
2025-09-26 19:03:48 -0500

    FEAT: Eliminado el home

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,12 +7,12 @@ export default function Layout() {
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-2xl font-bold">Gestión de Películas</h1>
           <ul className="flex space-x-4">
-            <li><Link to="/" className="hover:text-blue-400">Inicio</Link></li>
+            <li><Link to="/" className="hover:text-blue-400">Películas/Series</Link></li>
             <li><Link to="/generos" className="hover:text-blue-400">Géneros</Link></li>
             <li><Link to="/directores" className="hover:text-blue-400">Directores</Link></li>
             <li><Link to="/productoras" className="hover:text-blue-400">Productoras</Link></li>
             <li><Link to="/tipos" className="hover:text-blue-400">Tipos</Link></li>
-            <li><Link to="/media" className="hover:text-blue-400">Películas/Series</Link></li>
+            {/* <li><Link to="/media" className="hover:text-blue-400">Películas/Series</Link></li> */}
           </ul>
         </div>
       </nav>
